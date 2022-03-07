b2be88e66824366bea8bfa216d14aeb05df1f3da
Andrianjatovoniaina Andy <andy.andrianjatovoniaina@gmail.com>
2022-03-07 21:27:12 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/andya/tpcustomerapplication/session/CustomerManager.java
@@ -0,0 +1,32 @@
+package mg.andya.tpcustomerapplication.session;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import mg.andya.tpcustomerapplication.entities.Customer;
+
+/**
+ *
+ * @author andya
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+        return null;
+    }
+}
